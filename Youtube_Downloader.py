b736ebd7ed1eec7aec05f4f95410a97bfa63a63e
Exin <119764986+exinmood@users.noreply.github.com>
2024-03-01 00:37:30 +0330

    Add files via upload

---
+++ Youtube_Downloader.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+#create by Exin
+
+window = Tk()
+window.title("Youtube Downloader")
+window.minsize(450,170)
+window.maxsize(450,170)
+bn = Button(window  , text = "Link" , bg="white")
+bn.grid(row=4 , padx=30 , pady=35)
+lb = Label(window , text="Enter Your Link:")
+en = Entry(window , width=25)
+en.grid(padx=200 )
+
+bn2 = Button(window  , text = "Download" , bg="white")
+bn2.grid(row=30 , padx=30)
+
+window.mainloop()
+
